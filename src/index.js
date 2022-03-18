9d6f5457d34357a636ad364e1c643df42ebf2817
Kris <kris_s2@outlook.com>
2022-03-17 23:16:37 -0700

    Adding redux, and preparing for components to interact with redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+
+import reducer from './redux/reducers';
+import {createStore} from 'redux';
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
